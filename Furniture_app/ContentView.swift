543a574975a41304c37e17c9baf0ce49f9165218
Pink00Monster <vanillaWangyi@163.com>
2023-05-27 21:34:59 +0200

    Slogan

--- Furniture_app/ContentView.swift
+++ Furniture_app/ContentView.swift
@@ -13,8 +13,13 @@ struct ContentView: View {
             Color("Bg")
                 .edgesIgnoringSafeArea(.all)
            
-            VStack {
+            VStack(alignment: .leading) {
                 AppBarView()
+                
+                TagLineView()
+                    .padding()
+                
+                
             }
         }
         
@@ -47,5 +52,16 @@ struct AppBarView: View {
             }
         }
         .padding(.horizontal)
+    }
+}
+
+struct TagLineView: View {
+    var body: some View {
+        Text("Find The \nBest ")
+            .font(.custom("PlayfairDisplay-Regular", size: 28))
+            .foregroundColor(Color("Primary"))
+        + Text("Furniture!")
+            .font(.custom("PlayfairDisplay-Bold", size: 28))
+            .foregroundColor(Color("Primary"))
     }
 }

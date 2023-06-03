f3f560d7dc761b9ff0cef14fa24ae854d96a9ed0
Pink00Monster <vanillaWangyi@163.com>
2023-06-03 09:44:52 +0200

    Detail Screen

--- Furniture_app/ContentView.swift
+++ Furniture_app/ContentView.swift
@@ -12,23 +12,12 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            ZStack {
-                Color("Bg")
-                    .edgesIgnoringSafeArea(.all)
-                ScrollView {
-                    Image("chair_1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .edgesIgnoringSafeArea(.top)
-                    
-                    DescriptionView()
-                        .offset(y: -40.0)
-                
-                }
-            }
+            DetailScreen()
         }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -38,114 +27,4 @@ struct ContentView_Previews: PreviewProvider {
 
 
 
-struct DescriptionView: View {
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text("Swedish chair")
-                .font(.title)
-                .fontWeight(.bold)
-            
-            HStack(spacing: 4) {
-                ForEach(0 ..< 5) { item in
-                    Image("star")
-                }
-                Text("(4.9)")
-                    .opacity(0.5)
-                    .padding(.leading, 8)
-                Spacer()
-            }
-            Text("Description")
-                .fontWeight(.medium)
-                .padding(.vertical, 8)
-            
-            Text("Swedish chair is a contemporary chair based on the virtues of modern craft, it carries on the simplicities and honestly of the archetypical chair. ")
-                .lineSpacing(8.0)
-                .opacity(0.6)
-            
-            HStack (alignment: .top) {
-                VStack (alignment: .leading) {
-                    Text("Size")
-                        .fontWeight(.semibold)
-                        .padding(.bottom, 4)
-                    Text("Height: 120cm")
-                        .opacity(0.6)
-                    Text("Width: 80cm")
-                        .opacity(0.6)
-                    Text("Diameter: 72cm")
-                        .opacity(0.6)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                
-                VStack (alignment: .leading) {
-                    Text("Treatment")
-                        .fontWeight(.semibold)
-                        .padding(.bottom, 4)
-                    
-                    Text("Jati Wood, Canvas, \nAmazing Love")
-                        .opacity(0.6)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-            }
-            .padding(.vertical)
-            
-            HStack (alignment: .bottom) {
-                VStack (alignment: .leading) {
-                    Text("Colors")
-                        .fontWeight(.semibold)
-                    
-                    HStack {
-                        ColorDotView(color: .white)
-                        ColorDotView(color: .black)
-                        ColorDotView(color: .purple)
-                    }
-                }
-                
-                Spacer()
-                
-                VStack (alignment: .leading) {
-                    Text("Quantity")
-                        .fontWeight(.semibold)
-                    
-                    HStack {
-                        Button(action: {}) {
-                            Image(systemName: "minus")
-                                .padding(.all, 8)
-                        }
-                        .frame(width: 30, height: 30)
-                        .overlay(RoundedRectangle(cornerRadius: 50.0).stroke())
-                        .foregroundColor(.black)
-                        
-                        Text("1")
-                            .font(.title2)
-                            .fontWeight(.semibold)
-                            .padding(.horizontal, 8)
-                        
-                        Button(action: {}) {
-                            Image(systemName: "plus")
-                                .padding(.all, 8)
-                        }
-                        .frame(width: 30, height: 30)
-                        .background(Color("Primary"))
-                        .clipShape(Circle())
-                        .foregroundColor(.white)
-                    }
-                }
 
-            }
-            
-        }
-        .padding()
-        .padding(.top)
-        .background(Color("Bg"))
-        .cornerRadius(40.0)
-    }
-}
-
-struct ColorDotView: View {
-    let color: Color
-    var body: some View {
-        color
-            .frame(width: 24, height: 24)
-            .clipShape(Circle())
-    }
-}
